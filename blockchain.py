6d4926fef8a9c8036e1cf53859fe54d1079e9d00
adele-angel <adeler.ang@gmail.com>
2020-05-25 15:29:16 +0300

    If statement

---
+++ blockchain.py
@@ -0,0 +1,55 @@
+# Initializing blockchain list
+blockchain = []
+
+
+def get_last_blockchain_value():
+    """ Returns value of the current blockchain """
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction=[1]):
+    """ Append a new value as well as the last blockchin value to the block blockchin
+
+    Arguments:
+      :transaction_amount: The amount that should be added
+      :last_transaction: The last blockchain transaction (default [1])
+    """
+    blockchain.append([last_transaction, transaction_amount])
+
+
+def get_transaction_value():
+    """ Returns the input of the user (a new transaction amount) as a float """
+    user_input = float(input('Your transaction amount please: '))
+    return user_input
+
+
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
+
+
+def print_blockchain_elements():
+    # Output the blockchain list to console
+    for block in blockchain:
+        print('Outputting block')
+        print(block)
+
+
+# Get first transaction input and add the value to the blockchain
+tx_amount = get_transaction_value()
+add_value(tx_amount)
+
+
+while True:
+    print('Please choose')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    else:
+        print_blockchain_elements()
+
+
+print('Done!')
