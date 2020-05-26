2a71f991e77bcca92f5c7c1abfd6bb8ac132fcd7
adele-angel <adeler.ang@gmail.com>
2020-05-26 21:48:23 +0300

    all & any functions

--- blockchain.py
+++ blockchain.py
@@ -123,7 +123,7 @@ def mine_block():
 
 
 def get_transaction_value():
-    """ Returns the input of the user (a new transaction amount) as a float """
+    """ Returns the input of the user (a new transaction amount) as a float. """
     # Get user input, transform it from a string to a float and store it
     tx_recipient = input('Enter the recipient of the transaction: ')
     tx_amount = float(input('Your transaction amount please: '))
@@ -156,6 +156,11 @@ def verify_chain():
     return True
 
 
+def verify_transactions():
+    """ Verifies all open transactions. """
+    return all([verify_transaction(tx) for tx in open_transactions])
+
+
 waiting_for_input = True
 
 # A while loop for the user input interface
@@ -165,6 +170,7 @@ while waiting_for_input:
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
     print('4: Output participants')
+    print('5: Check transaction validity')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -184,6 +190,11 @@ while waiting_for_input:
         print_blockchain_elements()
     elif user_choice == '4':
         print(participants)
+    elif user_choice == '5':
+        if verify_transactions():
+            print('All transactions are valid')
+        else:
+            print('There are invalid transactions')
     elif user_choice == 'h':
         # Make sure that its impossible to "hack" the blockchain if its empty
         if len(blockchain) >= 1:
