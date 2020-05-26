5eb3fa6b4d1ad51b75ec53cbe600eafee5cb0e12
adele-angel <adeler.ang@gmail.com>
2020-05-26 14:18:01 +0300

    transactions with dictionaries and tuples

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,7 @@
 # Initializing blockchain list
 blockchain = []
+open_transactions = []
+owner = 'John'
 
 
 def get_last_blockchain_value():
@@ -9,22 +11,34 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_transaction(transaction_amount, last_transaction=[1]):
+def add_transaction(sender, recipient, amount=1.0):
     """ Append a new value as well as the last blockchin value to the block blockchin
 
     Arguments:
-      :transaction_amount: The amount that should be added
-      :last_transaction: The last blockchain transaction (default [1])
+      :sender: The sender of the coins.
+      :recipient: The recipient of the coins.
+      :amount: The amount of coins send with the transaction (default = 1.0).
     """
-    if last_transaction == None:
-        last_transaction = [1]
-    blockchain.append([last_transaction, transaction_amount])
+    transaction = {
+        'sender': sender,
+        'recipient': recipient,
+        'amount': amount
+    }
+    open_transactions.append(transaction)
+
+
+def mine_block():
+    pass
 
 
 def get_transaction_value():
     """ Returns the input of the user (a new transaction amount) as a float """
-    user_input = float(input('Your transaction amount please: '))
-    return user_input
+    # Get user input, transform it from a string to a float and store it
+    tx_recipient = input('Enter the recipient of the transaction: ')
+    tx_amount = float(input('Your transaction amount please: '))
+    # tuple, (tx_recipient, tx_amount)
+    # if tuple has more then 1 element, brackets can be omitted
+    return tx_recipient, tx_amount
 
 
 def get_user_choice():
@@ -64,11 +78,13 @@ while waiting_for_input:
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
-        tx_amount = get_transaction_value()
+        tx_data = get_transaction_value()
+        # Add the transaction amount to the blockchain
         add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
     elif user_choice == 'h':
+        # Make sure that its impossible to "hack" the blockchain if its empty
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     elif user_choice == 'q':
