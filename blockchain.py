50907d99e68eff03c40dbf9112e1447504203340
adele-angel <adeler.ang@gmail.com>
2020-05-28 18:02:45 +0300

    adding file access to the blockchain

--- blockchain.py
+++ blockchain.py
@@ -26,6 +26,25 @@ owner = 'John Doe'
 participants = {'John Doe'}
 
 
+def load_data():
+    with open('blockchain.txt', mode='r') as f:
+        file_content = f.readlines()
+        global blockchain
+        global open_transactions
+        blockchain = file_content[0]
+        open_transactions = file_content[1]
+
+
+load_data()
+
+
+def save_data():
+    with open('blockchain.txt', mode='w') as f:
+        f.write(str(blockchain))
+        f.write('\n')
+        f.write(str(open_transactions))
+
+
 def valid_proof(transactions, last_hash, proof):
     """ Validate a proof of work number and see if it solves the puzzle algorithm (two leading 0s).
 
@@ -117,6 +136,7 @@ def add_transaction(recipient, sender=owner,  amount=1.0):
         open_transactions.append(transaction)
         participants.add(sender)
         participants.add(recipient)
+        save_data()
         return True
     return False
 
@@ -142,6 +162,7 @@ def mine_block():
         'proof': proof
     }
     blockchain.append(block)
+    save_data()
     return True
 
 
